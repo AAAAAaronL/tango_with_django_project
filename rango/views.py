2ba4857c3319625f209c783eee4ac084549c8a0a
Zhaofeng Liu <2538238l@student.gla.ac.uk>
2021-07-29 16:09:53 +0800

    completed exercise for chapter 8

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
@@ -15,7 +15,7 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['extra'] = ''
+    
 
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -40,24 +40,27 @@ def show_category(request, category_name_slug):
 
 def add_category(request):
     form = CategoryForm()
+
     if request.method == 'POST':
         form = CategoryForm(request.POST)
+
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
-    return render(request, 'rango/add_category.html', {'form':form})
- 
+    
+    return render(request, 'rango/add_category.html', {'form': form})
+
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
     except:
         category = None
     
-    # You cannot add a page to a Category that does not exist... DM
+    
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
@@ -73,8 +76,7 @@ def add_page(request, category_name_slug):
 
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
-            print(form.errors)  # This could be better done; for the purposes of TwD, this is fine. DM.
+            print(form.errors) 
     
     context_dict = {'form': form, 'category': category}
-    return render(request, 'rango/add_page.html', context=context_dict)       
-  
+    return render(request, 'rango/add_page.html', context=context_dict)
